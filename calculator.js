e592d4de847d32aa839b3aecd5152f058ddecb53
Ice Maya <echaymaya@gmail.com>
2018-09-15 11:31:53 +1200

    created calculator.js

--- calculator.js
+++ calculator.js
@@ -1,4 +1,31 @@
-function showMe(num) {
-    document.input.show.value = document.input.show.value+num;   
+var initialVal = 0;
+var inputVal = 0;
+var outputVal = 0;
+
+
+// It should display whatever number is clicked
+function display(num) {
+    document.form.textview.value = document.form.textview.value+num;   
 }
 
+// It should have 'clear' button to reset the value to 0
+function clearVal(){
+    document.form.textview.value = '';
+}
+
+// It should calculate (add, substract, multiply and divide) numbers
+function calculate (){
+    var a = document.querySelector('input').value;
+
+}
+
+/* Requirements:
+ - Start at value zero
+ - collect input data from user
+ - rules: no zero digits as first value in a number, decimal can be use once in a row, only one operator can be use at once, 
+ - display input on screen
+ - perform calculation when use clicks equal '=' operator 
+ - display result 
+ - clear screen 
+
+// It should have input data storage to store input values
